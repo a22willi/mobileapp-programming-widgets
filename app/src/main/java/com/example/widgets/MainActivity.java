7c65bd524410c27f9f4ee37bd76c95513fb64eff
William Lindholm <a22willi@student.his.se>
2023-04-18 10:29:48 +0200

    Removed print.

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -35,7 +35,6 @@ public class MainActivity extends AppCompatActivity {
         sw1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                System.out.println(imgview.getVisibility());
                 if (imgview.getVisibility() == View.VISIBLE) {
                     imgview.setVisibility(View.INVISIBLE);
                 } else {

305a31bb7c56acf2f7be559856e8b72c9f554258
William Lindholm <a22willi@student.his.se>
2023-04-18 09:50:24 +0200

    Added listener on button that changes textview every time it is pressed.

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.widgets;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int ButtonPushedTimes = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        final TextView text = findViewById(R.id.text);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ButtonPushedTimes++;
+                text.setText("Button pressed: " + ButtonPushedTimes + " times");
+            }
+        });
     }
 }
